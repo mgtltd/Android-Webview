abadc0b7b6a507dc9e08f2094a9bbc65d727063a
mgtltd <87794406+mgtltd@users.noreply.github.com>
2024-09-02 11:58:25 +0800

    Add files via upload

---
+++ KINGSTONSTARCONSULTANTSLIMITED/app/src/main/java/com/totech/kingstonstarconsultantslimited/MainActivity.java
@@ -0,0 +1,90 @@
+package com.totech.kingstonstarconsultantslimited;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private ValueCallback<Uri[]> filePathCallback;
+    private final int FILE_CHOOSER_REQUEST_CODE = 1;
+
+    @SuppressLint({"SetJavaScriptEnabled", "MissingInflatedId"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url != null && (url.startsWith("http://") || url.startsWith("https://"))) {
+                    view.loadUrl(url); // Load all URLs in the WebView
+                }
+                else if (url.contains("m.me")) {
+                    view.loadUrl(url);
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    startActivity(intent);
+                }
+
+                return true;
+            }
+        });
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+                MainActivity.this.filePathCallback = filePathCallback;
+                openFilePicker();
+                return true;
+            }
+        });
+
+        // Load your URL
+        webView.loadUrl("https://lilmermaidshowroom.xyz");
+    }
+
+    private void openFilePicker() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("*/*");
+        startActivityForResult(intent, FILE_CHOOSER_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == FILE_CHOOSER_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK && data != null) {
+                if (filePathCallback != null) {
+                    filePathCallback.onReceiveValue(new Uri[]{data.getData()});
+                    filePathCallback = null;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
+
